d88dd0be5f3a01f898198db060d12cc218e85a4a
amoljoshi10 <89700319+amoljoshi10@users.noreply.github.com>
2021-11-15 18:19:40 +0530

    Update ProductsController.cs

--- ProductAPI/Controllers/ProductsController.cs
+++ ProductAPI/Controllers/ProductsController.cs
@@ -34,7 +34,19 @@ namespace DemoAPI.Controllers
         [HttpGet("{productId}")]
         public IActionResult GetById(int productId)
         {
+            Request.Headers.TryGetValue("x-security-header", out var headerValue);
 
+            // read your secret from Azure Key Vault
+            string kvUri =
+                    "https://kv-for-apim.vault.azure.net/";
+
+            SecretClient client =
+                new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            var secret= client.GetSecretAsync("securityHeader").Result.Value.Value;
+
+            if (!headerValue.Equals(secret))
+                return Unauthorized("Direct Access to API is restricted");
 
             var product=_productService.GetProductById(productId);
             if (product == null)
